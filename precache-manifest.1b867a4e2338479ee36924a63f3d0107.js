5ef8d8754b2cc115249c76f9707b6d61525e23cd
lehoangvuvt <hoangvule100@gmail.com>
2019-04-15 00:51:46 +0700

    Updates

--- precache-manifest.1b867a4e2338479ee36924a63f3d0107.js
+++ precache-manifest.1b867a4e2338479ee36924a63f3d0107.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "427d721b86fc9c68b2e85ad42b69238c",
-    "url": "/ghienphim/static/media/fa-regular-400.427d721b.woff"
+    "revision": "e6257a726a0cf6ec8c6fec22821c055f",
+    "url": "/ghienphim/static/media/fa-regular-400.e6257a72.woff2"
   },
   {
     "revision": "2e6545f32917bbc608f9",
@@ -20,52 +20,52 @@ self.__precacheManifest = [
     "url": "/ghienphim/static/media/fa-brands-400.a7b95dbd.eot"
   },
   {
-    "revision": "5e2f92123d241cabecf0b289b9b08d4a",
-    "url": "/ghienphim/static/media/fa-brands-400.5e2f9212.woff2"
-  },
-  {
     "revision": "2ef8ba3410dcc71578a880e7064acd7a",
     "url": "/ghienphim/static/media/fa-brands-400.2ef8ba34.woff"
   },
   {
-    "revision": "dcce4b7fbd5e895561e18af4668265af",
-    "url": "/ghienphim/static/media/fa-regular-400.dcce4b7f.eot"
+    "revision": "5e2f92123d241cabecf0b289b9b08d4a",
+    "url": "/ghienphim/static/media/fa-brands-400.5e2f9212.woff2"
   },
   {
     "revision": "98b6db59be947f563350d2284fc9ea36",
     "url": "/ghienphim/static/media/fa-brands-400.98b6db59.ttf"
   },
   {
-    "revision": "e6257a726a0cf6ec8c6fec22821c055f",
-    "url": "/ghienphim/static/media/fa-regular-400.e6257a72.woff2"
+    "revision": "dcce4b7fbd5e895561e18af4668265af",
+    "url": "/ghienphim/static/media/fa-regular-400.dcce4b7f.eot"
+  },
+  {
+    "revision": "427d721b86fc9c68b2e85ad42b69238c",
+    "url": "/ghienphim/static/media/fa-regular-400.427d721b.woff"
   },
   {
     "revision": "2e6545f32917bbc608f9",
     "url": "/ghienphim/static/js/main.bc6967dd.chunk.js"
   },
   {
-    "revision": "65b9977aa23185e8964b36eddbce7a20",
-    "url": "/ghienphim/static/media/fa-regular-400.65b9977a.ttf"
-  },
-  {
     "revision": "5bf145531213545e03ff41cd27df7d2b",
     "url": "/ghienphim/static/media/fa-brands-400.5bf14553.svg"
   },
   {
-    "revision": "5eb754ab7dbd2fee562360528db4c3c0",
-    "url": "/ghienphim/static/media/fa-regular-400.5eb754ab.svg"
+    "revision": "65b9977aa23185e8964b36eddbce7a20",
+    "url": "/ghienphim/static/media/fa-regular-400.65b9977a.ttf"
   },
   {
     "revision": "46e7cec623d8bd790d9fdbc8de2d3ee7",
     "url": "/ghienphim/static/media/fa-solid-900.46e7cec6.eot"
   },
   {
-    "revision": "a7140145ebaaf5fb14e40430af5d25c4",
-    "url": "/ghienphim/static/media/fa-solid-900.a7140145.woff"
+    "revision": "5eb754ab7dbd2fee562360528db4c3c0",
+    "url": "/ghienphim/static/media/fa-regular-400.5eb754ab.svg"
   },
   {
     "revision": "418dad87601f9c8abd0e5798c0dc1feb",
     "url": "/ghienphim/static/media/fa-solid-900.418dad87.woff2"
+  },
+  {
+    "revision": "a7140145ebaaf5fb14e40430af5d25c4",
+    "url": "/ghienphim/static/media/fa-solid-900.a7140145.woff"
   },
   {
     "revision": "ff8d9f8adb0d09f11d4816a152672f53",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ghienphim/precache-manifest.c344dde2b7289643d7c151562e3958f1.js"
+  "/ghienphim/precache-manifest.1b867a4e2338479ee36924a63f3d0107.js"
 );
 
 workbox.clientsClaim();
